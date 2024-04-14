9ef6c68d85c2d528ce9c77c9e6bbd2cb54c857b1
Abduljebar <abduljebar49@gmail.com>
2024-04-14 19:26:48 +0300

    typeorm, bycrpt, and jwt init

--- auth-microservice/src/app.module.ts
+++ auth-microservice/src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './auth/entities/user.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '12345678',
+      username: 'postgres',
+      entities: [User],
+      database: 'book-store',
+      synchronize: true,
+      logging: true,
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- auth-microservice/src/auth/dto/create-auth.dto.ts
+++ auth-microservice/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,20 @@
-export class CreateAuthDto {}
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+    MaxLength,
+    MinLength,
+  } from 'class-validator';
+  
+  export class CreateUserDto {
+    @IsString()
+    @MinLength(8)
+    @IsNotEmpty()
+    readonly password: string;
+  
+    @IsEmail()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly email: string;
+  }
+  
